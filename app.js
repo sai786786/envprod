644109056ec311c276674b6510e73b672daa2bf3
sai786786 <33508924+sai786786@users.noreply.github.com>
2018-05-25 18:03:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log(process.env.pair);
 
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end("Result will be visible in the console." + process.env.value + "," + process.env.key + "," + process.env.pair);
+    response.end("Result will be visible in the console." + process.env.value + "," + process.env.key + "," + process.env.pair + "," + process.env.PROJECT);
 }).listen(3000);
 
 
